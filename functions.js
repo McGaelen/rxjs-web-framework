1089bb29be10aa655dc3bf558fb703db92e055fb
McGaelen <mcinteeg@mail.gvsu.edu>
2025-05-14 23:18:26 -0400

    support Observable attributes, WIP: Observable child nodes

--- functions.js
+++ functions.js
@@ -1,26 +1,62 @@
 import {Observable} from "rxjs";
 
 /**
+ * TODO: this doesn't handle removing event listeners or unsubscribing from observables!
+ *
+ * @param tag {string}
+ * @param [attributes] {Record<string, Observable<string> | string | (() => (any | void))>}
+ * @param [children] {Array<string | HTMLElement | Observable<string | HTMLElement>>}
  * @returns HTMLDivElement
  */
-export function div(attributes, children) {
-  const ref = document.createElement('div')
+export function el(tag, attributes, children) {
+  const ref = document.createElement(tag)
 
-  Object.entries(attributes).forEach(([key, value]) => {
-    ref.setAttribute(key, value)
-  })
+  // don't actually need to save these on the ref object, but probably not
+  ref._attributeObservers = {}
+  ref._attributeSubscriptions = {}
 
-  children.forEach(child => {
-    if (typeof child === 'string') {
-      ref.appendChild(document.createTextNode(child))
-    } else {
-      ref.appendChild(child)
-    }
-  })
+  if (attributes) {
+    Object.entries(attributes).forEach(([key, value]) => {
+      if (typeof value === 'function') {
+        // if its a function, try to add it as an event listener
+        ref.addEventListener(key, value)
+      } else if (value.subscribe && typeof value.subscribe === 'function') {
+        // if it has a subscribe function, it's probably an Observable
+        ref._attributeObservers[key] = value
+      } else {
+        // otherwise, it is probably just a static value, so set it normally
+        ref.setAttribute(key, value)
+      }
+    })
+
+    // Subscribe to all observer attributes
+    Object.keys(ref._attributeObservers).forEach(key => {
+      ref._attributeSubscriptions[key] = ref._attributeObservers[key].subscribe(
+        val => ref.setAttribute(key, val)
+      )
+    })
+  }
+
+  if (children) {
+    // TODO: support Observables
+    children.forEach((child, idx) => {
+      if (child.subscribe && typeof child.subscribe === 'function') {
+        child.subscribe(val => {
+          ref.children[idx] = val
+        })
+      } else if (typeof child === 'string') {
+        ref.appendChild(document.createTextNode(child))
+      } else {
+        ref.appendChild(child)
+      }
+    })
+  }
+
+  return ref
 }
 
 /**
- * @returns {Observable<any> & {$set: (newVal: any) => void}}
+ * @returns {Observable<any> & { $set: (newVal: any) => void }}
  */
 export function state(initialVal) {
   let subscriber
