69f3462ba17c905018213988b6d602e5c68f3681
McGaelen <mcinteeg@mail.gvsu.edu>
2025-05-16 20:35:33 -0400

    fix child state not reacting, cleanup

--- functions.js
+++ functions.js
@@ -11,10 +11,6 @@ import {Observable, catchError, BehaviorSubject} from "rxjs";
 export function el(tag, attributes, children) {
   const ref = document.createElement(tag)
 
-  // don't actually need to save these on the ref object, but probably not
-  ref._attributeObservers = {}
-  ref._attributeSubscriptions = {}
-
   if (attributes) {
     Object.entries(attributes).forEach(([key, value]) => {
       if (typeof value === 'function') {
@@ -22,57 +18,36 @@ export function el(tag, attributes, children) {
         ref.addEventListener(key, value)
       } else if (value.subscribe && typeof value.subscribe === 'function') {
         // if it has a subscribe function, it's probably an Observable
-        ref._attributeObservers[key] = value
+        value.subscribe(
+          val => ref.setAttribute(key, val)
+        )
       } else {
         // otherwise, it is probably just a static value, so set it normally
         ref.setAttribute(key, value)
       }
     })
-
-    // Subscribe to all observer attributes
-    Object.keys(ref._attributeObservers).forEach(key => {
-      ref._attributeSubscriptions[key] = ref._attributeObservers[key].subscribe(
-        val => ref.setAttribute(key, val)
-      )
-    })
   }
 
   // TODO: i think all of `children` needs to be an observable to support conditionally rendering children...
   // TODO: use a tagged template for children that can create that observable
-  if (!children) return ref
-  if (tag === 'button') {
-    debugger
-  }
-
-  for (const [idx, child] of children.entries()) {
-    if (child.subscribe && typeof child.subscribe === 'function') {
-      // TODO: for some reason the subscribe method doesn't get called when the element reading it is a child?
-      child.subscribe({
-        next: val => {
-          if (tag === 'button') {
-            debugger
-          }
+  if (children) {
+    children.forEach((child, idx) => {
+      if (child.subscribe && typeof child.subscribe === 'function') {
+        child.subscribe(val => {
           const textNode = document.createTextNode(val?.toString() ?? val)
           if (ref.childNodes[idx]) {
             ref.replaceChild(textNode, ref.childNodes[idx])
           } else {
             ref.appendChild(textNode)
           }
-        },
-        complete() {
-          console.log('complete')
-        }
-      })
-    } else if (typeof child === 'string') {
-      ref.appendChild(document.createTextNode(child))
-    } else {
-      ref.appendChild(child)
-    }
+        })
+      } else if (typeof child === 'string') {
+        ref.appendChild(document.createTextNode(child))
+      } else {
+        ref.appendChild(child)
+      }
+    })
   }
-  // children.forEach((child, idx) => {
-  //
-  // })
-
 
   return ref
 }
